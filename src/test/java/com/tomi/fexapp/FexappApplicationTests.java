649d31761d5202b2b82967d1b5768f2632b30658
Tomislav2701 <tomislav.evlogiev4@gmail.com>
2025-03-25 20:52:14 +0200

    Initial commit: Spring Boot app

---
+++ src/test/java/com/tomi/fexapp/FexappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomi.fexapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FexappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
